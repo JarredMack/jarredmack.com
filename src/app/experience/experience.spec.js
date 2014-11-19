3e42a77c1b3a217b84f48d521492b7cf04c76c66
Jarred Mack <mackjarred@gmail.com>
2014-11-19 16:50:54 +1100

    Removing home module, and adding experience unit test

--- src/app/experience/experience.spec.js
+++ src/app/experience/experience.spec.js
@@ -5,10 +5,34 @@
  * automatically.
  */
 describe( 'experience section', function() {
-  beforeEach( module( 'jarredmack.experience' ) );
+    var $rootScope, $scope, controller;
 
-  it( 'should have a dummy test', inject( function() {
-    expect( true ).toBeTruthy();
-  }));
+    beforeEach( module( 'jarredmack.experience' ) );
+
+    beforeEach(inject(function(_$rootScope_, $controller, $q) {
+        $rootScope = _$rootScope_;
+        $scope = {};
+        var deferred = $q.defer();
+        var dummyAPI = {
+            get: function() {
+                deferred.resolve([{ data: "test" }]);
+                return deferred.promise;
+            }
+        };
+        controller = $controller('ExperienceCtrl', { $scope: $scope, JMApi: dummyAPI });
+    }));
+
+    it('should assign the results of JMApi.get to the scope', function() {
+        expect($scope.experience).toBeUndefined();
+        $rootScope.$apply();
+        expect($scope.experience).toBeDefined();
+        expect($scope.experience[0].data).toEqual("test");
+    });
+
+    it('should add a default visible property to all elements', function() {
+        $rootScope.$apply();
+        expect($scope.experience[0].visible).toBeDefined();
+        expect($scope.experience[0].visible).toEqual(false);
+    });
 });
 
