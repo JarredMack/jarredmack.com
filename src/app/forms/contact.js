f12696178b7409b084972cb4ce86d29157155f90
Jarred Mack <mackjarred@gmail.com>
2015-02-17 14:32:47 +1100

    Using cache service to maintain form state across routes

--- src/app/forms/contact.js
+++ src/app/forms/contact.js
@@ -1,8 +1,15 @@
 angular.module( 'form.contact', [])
 
-.controller( 'FormContactCtrl', ['$scope', function FormContactCtrl( $scope ) {
+.controller( 'FormContactCtrl', ['$scope', 'CacheService', function FormContactCtrl( $scope, CacheService ) {
 
-    $scope.query = {};
+    var cacheString = 'form:contact:query';
+    CacheService.fetch(cacheString)
+        .then(function(query) {
+            $scope.query = query;
+        }, function() {
+            $scope.query = {};
+            CacheService.add(cacheString, $scope.query);
+        });
 
     $scope.submit = function() {
         console.log($scope.query, $scope.formContact);
